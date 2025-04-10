a79f9503ec9bd49bbe6d2059c1957c18c47ef9b9
SantosNovoa <novoasj8729@student.laccd.edu>
2025-04-10 19:53:34 -0400

    Working operate function.
    Accepts an operator +, -, *, / and two numbers. Then calls one of the add, subtract, multiply, or divide functions depending on the operator.

--- script.js
+++ script.js
@@ -1,21 +1,39 @@
-console.log("hello world!")
+console.log("hello world!");
+let numOne;
+let numTwo;
+let operator;
 
-function add(numOne, numTwo){
-    return numOne + numTwo;
-};
+function add(numOne, numTwo) {
+  return numOne + numTwo;
+}
 
 function subtract(numOne, numTwo) {
-    return numOne - numTwo;
+  return numOne - numTwo;
 }
 
 function multiply(numOne, numTwo) {
-    return numOne * numTwo;
+  return numOne * numTwo;
 }
 
 function divide(numOne, numTwo) {
-    let num = 0;
-    num = numOne / numTwo;
-    let roundedNum = num.toFixed(2);
-    return roundedNum;
+  return numOne / numTwo;
 }
 
+function operate(operator, numOne, numTwo) {
+  let result;
+  switch (operator) {
+    case "+":
+      result = add(numOne, numTwo);
+      break;
+    case "-":
+      result = subtract(numOne, numTwo);
+      break;
+    case "*":
+      result = multiply(numOne, numTwo);
+      break;
+    case "/":
+      result = divide(numOne, numTwo);
+      break;
+  }
+  return result;
+}
