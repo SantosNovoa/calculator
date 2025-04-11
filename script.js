f575d8923d028ae636ecf7f888be428eb4531426
SantosNovoa <novoasj8729@student.laccd.edu>
2025-04-11 19:16:58 -0400

    Created functiions and event listeners that allow buttons pressed on the calculator to be appended to the screen

--- script.js
+++ script.js
@@ -1,7 +1,42 @@
-console.log("hello world!");
+const screen = document.querySelector("span");
+const clearBtn = document.querySelector(".clear-button");
+const numBtns = document.querySelectorAll("[data-number");
+const operatorBtns = document.querySelectorAll("[data-operator]");
+console.log(operatorBtns);
+console.log(numBtns);
+console.log(clearBtn);
+console.log(screen);
 let numOne;
 let numTwo;
 let operator;
+let result;
+
+clearBtn.addEventListener("click", () => {
+  screen.textContent = ""
+  result = 0;
+  console.log(result);
+})
+
+
+numBtns.forEach(button => {
+  button.addEventListener("click", () => {
+    appendNumber(button.textContent);
+  })
+})
+
+operatorBtns.forEach(button => {
+  button.addEventListener("click", () => {
+    setOperator(button.textContent);
+  })
+})
+
+function appendNumber(number) {
+  screen.textContent += number;
+}
+
+function setOperator(operator) {
+  screen.textContent += operator;
+}
 
 function add(numOne, numTwo) {
   return numOne + numTwo;
@@ -20,20 +55,37 @@ function divide(numOne, numTwo) {
 }
 
 function operate(operator, numOne, numTwo) {
-  let result;
+  result = 0;
   switch (operator) {
     case "+":
-      result = add(numOne, numTwo);
+      result += add(numOne, numTwo);
+      screen.textContent = result;
       break;
     case "-":
-      result = subtract(numOne, numTwo);
+      result += subtract(numOne, numTwo);
+      screen.textContent = result;
       break;
     case "*":
-      result = multiply(numOne, numTwo);
+      result += multiply(numOne, numTwo);
+      screen.textContent = result;
       break;
     case "/":
-      result = divide(numOne, numTwo);
+      if (numTwo === 0) {
+        screen.textContent = "Error.";
+      } else {
+        result += divide(numOne, numTwo);
+        screen.textContent = result.toFixed(2);
+      }
       break;
   }
   return result;
 }
+
+// operate("+", 40, 6)
+// console.log(result)
+// operate("-", 2, 3)
+// console.log(result)
+// operate("*", 2, 3)
+// console.log(result)
+// operate("/", 2, 3)
+// console.log(result)
